61bd1fad0c7b5a0436e81f5cbf5ab9be7437ee0d
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2022-02-09 11:10:17 +0100

    feat: Se hace responsive la pagina

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -7,7 +7,7 @@ import './App.css'
 function App () {
   return (
     <TodosProvider>
-      <div className="app w-8/12 mx-auto text-center mt-[-7px]">
+      <div className="app w-11/12 md:w-8/12 lg:w-6/12 mx-auto text-center mt-[-7px]">
         <p className="app__title font-thin text-[100px]">todos</p>
         <div className="todos border-b border-b-[#e6e6e6] mt-[-12px] divide-y divide-[#e6e6e6]">
           <AddTodo />
