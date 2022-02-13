792a0ef5d63cb61c0943dca914edb1dd4eaa0476
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2022-02-13 14:58:01 +0100

    perf: se evitó renderizados innecesarios del componente App

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,19 +1,21 @@
-import { AddTodo } from 'components/add-todo/AddTodo'
-import { Footer } from 'components/Footer'
-import { TodosList } from 'components/todos-list/TodosList'
-import { TodosFilterProvider } from 'contexts/todos-filter/todosFilter'
+import { Footer } from 'components/footer'
+import { Todos } from 'components/todos'
 import { TodosProvider } from 'contexts/todos/todos'
 import './App.css'
 
 function App () {
   return (
-    <TodosProvider>
+    <>
       <div className="app w-11/12 md:w-8/12 lg:w-6/12 mx-auto text-center mt-[-7px]">
         <p className="app__title font-thin text-[100px]">todos</p>
+
+        <TodosProvider>
           <Todos />
+        </TodosProvider>
+
         <Footer />
       </div>
-    </TodosProvider>
+    </>
   )
 }
 
