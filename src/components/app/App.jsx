650211f75f1d98289648c82537d0e8e69a5096c2
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2022-02-13 15:41:48 +0100

    chore: Se movio el componente Footer

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,4 @@
-import { Footer } from 'components/footer'
+import { Footer } from 'components/app/footer'
 import { Todos } from 'components/todos'
 import { TodosProvider } from 'contexts/todos/todos'
 import './App.css'
