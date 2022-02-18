7e641d4a50a0b52eab6afb363d7c8f317d380f7e
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2022-02-18 17:10:47 +0100

    refactor: se extrae la logica y la llamada al contexto de los Todos
    se extrae la logica y la llamada al contexto de los Todos aun custom hook

--- src/components/todos/todos-list/TodoList.test.jsx
+++ src/components/todos/todos-list/TodoList.test.jsx
@@ -1,19 +1,28 @@
+import { randText } from '@ngneat/falso'
 import { cleanup, render } from '@testing-library/react'
+import { useTodosContext } from 'hooks/useTodosContext'
+import { nanoid } from 'nanoid'
 import { useContext } from 'react'
 import { afterEach, describe, expect, test, vi } from 'vitest'
 import { TodosList } from './TodosList'
 
-vi.mock('react', async () => ({
-  ...(await vi.importActual('react')),
-  useContext: vi.fn()
-}))
+vi.mock('react', async () => {
+  const actual = await vi.importActual('react')
+
+  return {
+    ...actual,
+    useContext: vi.fn()
+  }
+})
+
+vi.mock('hooks/useTodosContext', () => ({ useTodosContext: vi.fn() }))
 
 const renderComponent = () => render(<TodosList />)
 
 const defaultState = [
-  { isCompleted: true, id: '1', value: 'Boba Fett' },
-  { isCompleted: true, id: '2', value: 'Boba Fett' },
-  { isCompleted: false, id: '3', value: 'Boba Fett' }
+  { isCompleted: true, id: nanoid(), value: randText() },
+  { isCompleted: true, id: nanoid(), value: randText() },
+  { isCompleted: false, id: nanoid(), value: randText() }
 ]
 
 describe('Pruebas sobre el componente <TodoList/>', () => {
@@ -21,38 +30,39 @@ describe('Pruebas sobre el componente <TodoList/>', () => {
     cleanup()
   })
 
-  test.concurrent('debe no mostrar nada cuando no hay todos', () => {
-    useContext.mockReturnValue({
-      state: []
-    })
+  test('debe no mostrar nada cuando no hay todos', () => {
+    useTodosContext.mockReturnValue({ state: [] })
+    useContext.mockReturnValue({ todosFilter: undefined })
+
     const component = renderComponent()
+
     expect(component.container.outerHTML).toBe('<div></div>')
   })
 
-  test.concurrent('debe mostrar todos los Todos cuando el valor del todosFilter sea undefined', () => {
-    useContext.mockReturnValue({
-      state: defaultState,
-      todosFilter: undefined
-    })
+  test('debe mostrar todos los Todos cuando el valor del todosFilter sea undefined', () => {
+    useTodosContext.mockReturnValue({ state: defaultState })
+    useContext.mockReturnValue({ todosFilter: undefined })
+
     renderComponent()
+
     expect(document.getElementsByTagName('li').length).toBe(3)
   })
 
   test('debe mostrar solo los Todos completados cuando el valor del todosFilter sea True', () => {
-    useContext.mockReturnValue({
-      state: defaultState,
-      todosFilter: true
-    })
+    useTodosContext.mockReturnValue({ state: defaultState })
+    useContext.mockReturnValue({ todosFilter: true })
+
     renderComponent()
+
     expect(document.getElementsByTagName('li').length).toBe(2)
   })
 
-  test.concurrent('debe mostrar solo los Todos no completados cuando el valor del todosFilter sea False', () => {
-    useContext.mockReturnValue({
-      state: defaultState,
-      todosFilter: false
-    })
+  test('debe mostrar solo los Todos no completados cuando el valor del todosFilter sea False', () => {
+    useTodosContext.mockReturnValue({ state: defaultState })
+    useContext.mockReturnValue({ todosFilter: false })
+
     renderComponent()
+
     expect(document.getElementsByTagName('li').length).toBe(1)
   })
 })
